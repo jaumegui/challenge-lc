41be8699f604875422f27d924430bfc51bc92509
Guillaume Ecollan <guillaume@ecollan.fr>
2019-06-20 15:25:00 +0200

    back button

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { compose } from "redux";
+import { Link } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 import { Typography } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 import { deepPurple } from '@material-ui/core/colors';
@@ -32,7 +34,10 @@ const styles = {
   },
   grid: {
     justifyContent: "space-between"
-  }
+  },
+  link: {
+    textDecoration: 'none'
+  },
 }
 
 
@@ -71,7 +76,18 @@ class Dashboard extends Component {
       return( 
         <div className={classes.root}>
           <div style={styles.title}>
-            <Typography variant="h3" gutterBottom>{details.name}</Typography>
+            <div style={styles.title}>
+              <Link to={'/operators'} style={styles.link}>
+                <Button 
+                  variant="outlined" 
+                  color="default" 
+                  className={classes.button} 
+                  style={{marginRight: "20px"}}>
+                    Retour
+                </Button>
+              </Link>
+              <Typography variant="h3" gutterBottom>{details.name}</Typography>
+            </div>
             <Avatar className={classes.purpleAvatar} size={"300"}>{details.score}</Avatar>
           </div>
           <h3>
